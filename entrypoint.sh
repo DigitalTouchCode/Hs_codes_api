995c7536c45ad0423efe589e7562a227dedbfc09
casper moyo <cassymyo@gmail.com>
2026-07-03 11:00:43 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-core.settings.production}"
+
+echo "Using settings: $DJANGO_SETTINGS_MODULE"
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 
